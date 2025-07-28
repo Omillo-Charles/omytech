19ff200847c9884609cb5ba5f4d3cf2acbb2fd6d
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-07-28 12:25:39 +0300

    implment web analytics on vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
